890b05ea7f39ea38783e591b0e71a3b4abb5787f
Reza Alfara <reza.alfara07@gmail.com>
2022-03-16 12:52:12 +0700

    Upload Pertama

---
+++ app/src/main/java/com/rezalfara/tugasakhir/GantiPasswordActivity.kt
@@ -0,0 +1,11 @@
+package com.rezalfara.tugasakhir
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GantiPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ganti_password)
+    }
+}
